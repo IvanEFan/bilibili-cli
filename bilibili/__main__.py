c710e15287d96d0b708b0a55a0b3e78b1e916cfa
IvanEFan <ivan1efan@gmail.com>
2021-05-21 14:25:45 +0800

    click options

--- bilibili/__main__.py
+++ bilibili/__main__.py
@@ -3,10 +3,18 @@ from bilibili import data
 from bilibili.renderer import ListLayoutRenderer, TableLayoutRenderer
 
 @click.command()
-def cli():
+@click.option(
+    '--layout',
+    '-L',
+    type=click.Choice(['list', 'table'], case_sensitive=False),
+    help='The output format (list or table), default is list'
+)
+def cli(layout):
+    renderer = ListLayoutRenderer()
+    if layout == 'table':
+        renderer = TableLayoutRenderer()
+    renderer.render_videos(data.get_rank())
     pass
 
 if __name__ == '__main__':
-    renderer = TableLayoutRenderer()
-    renderer.render_videos(data.get_rank())
     cli()
